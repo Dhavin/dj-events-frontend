21328fed50e1c224e282e1676ac1ec6ee45fa740
Dhavin <dhavin0809@gmail.com>
2021-05-12 23:27:55 +0530

    last commit

---
+++ components/Showcase.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+    return (
+        <div className={styles.showcase}>
+            <h1>Welcome to the Party!</h1> 
+            <h2>Find the Coolest DJ Events!</h2>           
+        </div>
+    )
+}
+ 
